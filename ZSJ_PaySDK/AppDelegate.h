8ffd11ada36d4548f94bd002b27f764e2c764fca
周双建 <1542100658@qq.com>
2016-01-08 09:22:18 +0800

    Initial Commit

---
+++ ZSJ_PaySDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZSJ_PaySDK
+//
+//  Created by 周双建 on 16/1/8.
+//  Copyright © 2016年 周双建. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZSJ_PaySDKUITests/ZSJ_PaySDKUITests.m
@@ -0,0 +1,40 @@
+//
+//  ZSJ_PaySDKUITests.m
+//  ZSJ_PaySDKUITests
+//
+//  Created by 周双建 on 16/1/8.
+//  Copyright © 2016年 周双建. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZSJ_PaySDKUITests : XCTestCase
+
+@end
+
+@implementation ZSJ_PaySDKUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
